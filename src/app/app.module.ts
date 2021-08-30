529f3fe908978f1fd8b8bef2a4d4337864c1731d
Kauã Nunes <kauanunnes@hotmail.com>
2021-08-30 11:40:38 -0300

    service contato criada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './views/menu/menu.component';
+import { ListaDeContatosComponent } from './views/lista-de-contatos/lista-de-contatos.component';
+import { ContatoComponent } from './views/contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ListaDeContatosComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
