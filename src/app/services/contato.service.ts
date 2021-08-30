d2ab7eff4a371f47787325ea0da283552d96d8ed
Kauã Nunes <kauanunnes@hotmail.com>
2021-08-30 12:07:45 -0300

    service finalizada

--- src/app/services/contato.service.ts
+++ src/app/services/contato.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Contato } from '../models/Contato';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContatoService {
+  private contatos: Contato[] = [
+    {
+      nome: 'Kauã Nunes',
+      email: 'kauanunes@gmail.com',
+      tels: ['555-5555', '888-8888'],
+    },
+    {
+      nome: 'Kaylane Nunes',
+      email: 'kaylane@gmail.com',
+      tels: ['555-5555'],
+    },
+    {
+      nome: 'Maria Nunes',
+      email: 'maria@gmail.com',
+      tels: ['555-5555'],
+    },
+    {
+      nome: 'Tereza Nunes',
+      email: 'tereza@gmail.com',
+      tels: ['555-5555'],
+    },
+    {
+      nome: 'João Nunes',
+      email: 'joao@gmail.com',
+      tels: ['555-5555'],
+    },
+    {
+      nome: 'Mariana Nunes',
+      email: 'mari@gmail.com',
+      tels: ['555-5555'],
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
+  getContatos():Contato[] {
+    return this.contatos;
+  }
 }
--- src/app/views/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/views/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Contato } from 'src/app/models/Contato';
+import { ContatoService } from 'src/app/services/contato.service';
 
 @Component({
   selector: 'app-lista-de-contatos',
@@ -7,37 +8,10 @@ import { Contato } from 'src/app/models/Contato';
   styleUrls: ['./lista-de-contatos.component.css'],
 })
 export class ListaDeContatosComponent implements OnInit {
-  contatos: Contato[] = [
-    {
-      nome: 'Kauã Nunes',
-      email: 'kauanunes@gmail.com',
-      tels: ['555-5555', '888-8888']
-    },
-    {
-      nome: 'Kaylane Nunes',
-      email: 'kaylane@gmail.com',
-      tels: ['555-5555']
-    },
-    {
-      nome: 'Maria Nunes',
-      email: 'maria@gmail.com',
-      tels: ['555-5555']
-    },
-    {
-      nome: 'Tereza Nunes',
-      email: 'tereza@gmail.com',
-      tels: ['555-5555']
-    },
-    {
-      nome: 'João Nunes',
-      email: 'joao@gmail.com',
-      tels: ['555-5555']
-    },
-  ];
-
-  constructor() {}
-
-  ngOnInit(): void {
-    console.log(this.contatos)
+  contatos?: Contato[]
+  constructor(private contatoService: ContatoService) {
+    this.contatos = contatoService.getContatos()
   }
+
+  ngOnInit(): void {}
 }
