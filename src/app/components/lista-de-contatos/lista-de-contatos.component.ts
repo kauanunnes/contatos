cca1b119497a014c1e03dc4f15f4495fd5a4f227
Kauã Nunes <kauanunnes@hotmail.com>
2021-09-07 19:19:11 -0300

    Adicionando contatos e recarregando o lista de contatos

--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -5,18 +5,18 @@ import { ContatoService } from 'src/app/services/contato.service';
 @Component({
   selector: 'app-lista-de-contatos',
   templateUrl: './lista-de-contatos.component.html',
-  styleUrls: ['./lista-de-contatos.component.css']
+  styleUrls: ['./lista-de-contatos.component.css'],
 })
 export class ListaDeContatosComponent implements OnInit {
-
   public contatos: Contato[];
-  private cs:ContatoService = new ContatoService();
+  private cs: ContatoService = new ContatoService();
 
   constructor() {
     this.contatos = this.cs.getContatos();
+    ContatoService.onContactsChanged.subscribe((data) => {
+      this.contatos = data;
+    });
   }
 
-  ngOnInit(): void {
-  }  
-
+  ngOnInit(): void {}
 }
